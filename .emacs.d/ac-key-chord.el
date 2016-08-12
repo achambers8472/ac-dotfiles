58ef89a043add99fed36de1dccf754d7cf8ee209
Alexander Chambers <alexander.chambers@hotmail.com>
2016-08-12 16:34:24 -0400

    Add keybinding.

--- .emacs.d/ac-key-chord.el
+++ .emacs.d/ac-key-chord.el
@@ -1,6 +1,16 @@
+;;; ac-key-chord --- Settings for key-chord package
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'key-chord)
 (setq key-chord-two-keys-delay 0.2)
 (key-chord-mode 1)
 
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-define evil-replace-state-map "jk" 'evil-normal-state)
+(key-chord-define evil-visual-state-map ",c" 'comment-dwim)
+
+(provide 'ac-key-chord)
+;;; ac-key-chord.el ends here
