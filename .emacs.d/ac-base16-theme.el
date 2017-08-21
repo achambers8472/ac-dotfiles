dcc4c5d138b16741e646e8275a8dcbaf4cf18c0c
Alexander Chambers <alexander.chambers@hotmail.com>
2017-08-21 12:45:07 +0930

    Update a tonne of emacs stuff.

--- .emacs.d/ac-base16-theme.el
+++
@@ -1,1 +0,0 @@
-(load-theme 'base16-default-dark t)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,112 @@
-(when (or (string= system-name "kraft")
-	  (string= system-name "frost")
-          (string= system-name "qai"))
-  (load "~/.emacs.d/ac-packages.el"))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
 
-(load "~/.opt/ac-essentials/submodules/solarized/emacs-colors-solarized/color-theme-solarized.el")
+(require 'use-package)
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode t)
+  (define-key evil-insert-state-map (kbd "<return>") 'evil-ret-and-indent)
+  (defun insert-line-below ()
+    (interactive)
+    (save-excursion
+      (evil-open-below 1)
+      (evil-normal-state)
+      )
+    )
+  (define-key evil-normal-state-map (kbd "<return>") 'insert-line-below)
+  (define-key evil-motion-state-map "H" 'evil-beginning-of-line)
+  (define-key evil-motion-state-map "L" 'evil-end-of-line)
+  )
+
+(use-package key-chord
+  :ensure t
+  :config
+  (setq key-chord-two-keys-delay 0.2)
+  (key-chord-mode t)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+  (key-chord-define evil-replace-state-map "jk" 'evil-normal-state)
+  (key-chord-define evil-visual-state-map ",c" 'comment-dwim)
+  )
+
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode t)
+  )
+
+(use-package ido
+  :ensure t
+  :config
+  (ido-mode t)
+  (ido-everywhere t)
+  )
+
+(use-package ido-vertical-mode
+  :ensure t
+  :config
+  (ido-vertical-mode t)
+  (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+)
+
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  (ido-ubiquitous-mode t)
+  )
+
+(use-package smex
+  :ensure t
+  :config
+  (smex-initialize)
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+  )
+
+(use-package magit
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode)
+  )
+
+(use-package fill-column-indicator
+  :ensure t)
+
+(use-package color-theme
+  :ensure t
+  )
+
+(load-file "~/.opt/ac-essentials/submodules/solarized/emacs-colors-solarized/color-theme-solarized.el")
 (color-theme-solarized-light)
+
+(use-package tex
+  :ensure auctex
+  :config
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq TeX-newline-function 'newline-and-indent)
+  (add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+  (add-hook 'LaTeX-mode-hook 'reftex-mode)
+  (setq-default TeX-master nil)
+  )
+
+(use-package reftex
+  :config
+  (setq reftex-use-external-file-finders t)
+  (setq reftex-external-file-finders
+        '(("tex" . "kpsewhich -format=.tex %f")
+          ("bib" . "kpsewhich -format=.bib %f")))
+  )
+
+(use-package json)
+
 
 (tool-bar-mode nil)
 
